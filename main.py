4e6a5dd33ed737d42f6dc9d50154c52830aaf6aa
PengYue <notch15@outlook.com>
2024-11-26 23:34:45 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import flet as ft
 
+def main(page):
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        new_task.focus()
+        new_task.update()
 
-def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
-
+    new_task = ft.TextField(hint_text="What's needs to be done?", width=300)
+    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
 
 ft.app(main)
