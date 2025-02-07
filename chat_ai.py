1d6be9e4f6d5441cb7e48f9402887f6cba463ce1
Yue Peng <30287633+Edward-Yue-Peng@users.noreply.github.com>
2025-02-07 16:16:13 +0800

    hide API key

--- chat_ai.py
+++ chat_ai.py
@@ -1,16 +1,18 @@
 import os
 from openai import OpenAI
 
+api = "YOUR_API"
 client = OpenAI(
-    api_key="sk-061fdeb4bb784fa2b097d4413ea6ac5f",
-    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    api_key=api,
+    base_url="YOUR_BASE",
 )
 
 
 def chatai(input_prompt):
-    completion = client.chat.completions.create(
-        model="qwen-turbo", messages=[{"role": "user", "content": input_prompt}]
-    )
-    return completion.choices[0].message.content
-
-print(chatai('hello'))
+    try:
+        completion = client.chat.completions.create(
+            model="qwen-turbo", messages=[{"role": "user", "content": input_prompt}]
+        )
+        return completion.choices[0].message.content
+    except:
+        return "API not configured."
